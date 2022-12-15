e43e0cef5390300861d5671e07e8c53d058388ee
Russino <manuelrusso070@gmail.com>
2022-12-15 19:52:24 +0100

    Integrazione Es Drone

--- ARG_JSON/parserJSON/DroneJson/drone/src/main/java/org/example/Main.java
+++ ARG_JSON/parserJSON/DroneJson/drone/src/main/java/org/example/Main.java
@@ -11,9 +11,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Drone> posizioni = new ArrayList<>();
-        List<Object> longitudiniList = new ArrayList<>();
-        List<Object> latitudiniList = new ArrayList<>();
-        List<Object> altitudiniList = new ArrayList<>();
 
         try {
             ObjectMapper mapper=new ObjectMapper();
@@ -21,18 +18,9 @@ public class Main {
             JsonNode rootNodes = node.path("percorso");
             JsonNode posizioneNode = rootNodes.path("posizione");
             for (JsonNode posizione: posizioneNode) {
-                for (int i = 0; i<longitudiniList.size(); i++)  {
-                    longitudiniList.add(posizione.get("longitudine").asInt());
-                }
-
-                for (int i = 0; i<latitudiniList.size(); i++)  {
-                    latitudiniList.add(posizione.get("latitudine").asInt());
-                }
-
-                for (int i = 0; i<altitudiniList.size(); i++)  {
-                    altitudiniList.add(posizione.get("altitudine").asInt());
-                }
-
+                int longitudine = posizione.get("longitudine").asInt();
+                int latitudine = posizione.get("latitudine").asInt();
+                int altitudine = posizione.get("altitudine").asInt();
                 posizioni.add(new Drone(longitudine,latitudine,altitudine));
             }
             for (Drone spostamenti : posizioni) {
